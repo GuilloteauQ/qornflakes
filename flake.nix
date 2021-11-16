1abeb55a0887ab9bc23ecf8fb2fb3f035088d8af
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2021-11-16 15:21:59 +0100

    globus connect personal: ok but gui not working ...

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
         recorder-viz = import ./pkgs/recorder-viz/default.nix { inherit pkgs; };
         globus-cli = import ./pkgs/globus-cli/default.nix { inherit pkgs; qorn_globus_sdk = self.packages.x86_64-linux.globus-sdk; };
         globus-sdk = import ./pkgs/globus-sdk/default.nix { inherit pkgs; };
+        globus-connect-personal = import ./pkgs/globus-connect-personnal/default.nix { inherit pkgs; };
       };
     };
 }
---
+++ pkgs/globus-connect-personnal/default.nix
@@ -0,0 +1,37 @@
+{ pkgs }:
+with pkgs;
+pkgs.tcl.mkTclDerivation {
+# stdenv.mkDerivation {
+  name = "globus-connect-personal";
+  src = fetchTarball {
+  url = "https://downloads.globus.org/globus-connect-personal/linux/stable/globusconnectpersonal-latest.tgz";
+  sha256 = "1b3q30nc4ay6y1ac3sdzbcckq7gfy3bzhxr5vvamddxgrk7hi4b7";
+};
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    audit
+    glibc
+    gcc-unwrapped
+    tk
+    tcllib
+    tcl
+    tclx
+  ];
+  buildInputs = [
+    tk tcllib tcl tclx
+  ];
+
+  propagatedBuildInputs = [ tk ];
+  buildPhase = ''
+    mkdir -p $out/bin
+    cp -r * $out
+    ln -s $out/globusconnectpersonal $out/bin/globusconnectpersonal
+    ln -s $out/globusconnect $out/bin/globusconnect
+    '';
+  installPhase = ''
+    true
+    '';
+
+
+}
