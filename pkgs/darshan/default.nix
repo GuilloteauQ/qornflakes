3d22ed697ac44dd7921311b579229fd29426456f
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2021-11-17 19:02:08 +0100

    darshan and pydarshan

--- pkgs/darshan/default.nix
+++ pkgs/darshan/default.nix
@@ -9,7 +9,7 @@ let
     sha256 = "sha256-5rGsA21GrMg7b3UGi1ShJHX/lmfXA01nY6ZTacNgZRA";
   };
 in
-  {
+  rec {
     pydarshan = python38Packages.buildPythonPackage rec {
       name = "pydarshan";
       version = "0.1";
@@ -21,8 +21,34 @@ in
         matplotlib
         pytest
         pytest-runner
+        darshan-util
       ];
       doCheck = false;
+    };
+    darshan-util = stdenv.mkDerivation {
+      name = "darshan-util";
+      version = "3.3";
+      src = "${rootSrc}";
+      buildInputs = [
+        coreutils
+        automake
+        autobuild
+        autoconf
+        libtool
+        zlib
+        python38
+        bzip2
+        libarchive
+      ];
+      buildPhase = ''
+        mkdir -p $out/build
+        cd darshan-util
+        autoreconf -fi
+        ./configure --prefix=$out --with-zlib=${pkgs.zlib.dev} --without-bzlib
+
+        make
+        make install
+      '';
     };
   }
 
