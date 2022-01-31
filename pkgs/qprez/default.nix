5e141cdc47d1ae4a046e005ee78243e4eda53e84
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-01-31 09:16:37 +0100

    update qprez

--- pkgs/qprez/default.nix
+++ pkgs/qprez/default.nix
@@ -7,8 +7,10 @@ rPackages.buildRPackage rec {
   src = fetchFromGitHub {
     owner = "GuilloteauQ";
     repo = "QPrez";
-    rev = "35a7010c8d5bf06234d6ce8d57dcaa2c6b3f0ffa";
-    sha256 = "sha256-CQtAogmEoniinSqByA1k0N3pyIhOvD6OjPrzw+4dZAo";
+    # rev = "35a7010c8d5bf06234d6ce8d57dcaa2c6b3f0ffa";
+    rev = "5eea5311ba04ac411876a0b2f12fe9a0b95d7363";
+    # sha256 = "sha256-CQtAogmEoniinSqByA1k0N3pyIhOvC6OjPrzw+4dZAo";
+    sha256 = "sha256-1/nq1qKLw2/1nqDnLdOvC8XTRwvSZBbMyoatpg6C6eA";
   };
   buildInputs = with rPackages; [ R rmarkdown knitr R_utils ];
 }
