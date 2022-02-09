0d0cb944bc8174ceb942c177aa8beb909cabcc55
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-02-09 20:42:20 +0100

    encore des sha ?

--- pkgs/jless/default.nix
+++ pkgs/jless/default.nix
@@ -8,9 +8,9 @@ rustPlatform.buildRustPackage rec {
     owner = "PaulJuliusMartinez";
     repo = pname;
     rev = "b3f21a215a9f3b12156f542cc78f330624d5faf1";
-    sha256 = "sha256-gBqyo/N/qF6HCTUrSKNVLiL1fc/JTfip1kNpNCBzRT8";
+    sha256 = "sha256-gBqyo/N/qF6HCTUrSKNVLiL1fc/JTfip1kNpNCBzRT8=";
   };
 
-  cargoSha256 = "sha256-eG9lxUhcIC/VrS0afHDxyJf3ffRGEK97pQGzawzASBk";
+  cargoSha256 = "sha256-eG9lxUhcIC/VrS0afHDxyJf3ffRGEK97pQGzawzASBk=";
   verifyCargoDeps = true;
 }
