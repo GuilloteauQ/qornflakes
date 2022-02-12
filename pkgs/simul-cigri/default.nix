297459ac4c550052a4d6aeea01103266378ba431
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-02-12 03:57:31 +0100

    docker for cigri-simul

--- pkgs/simul-cigri/default.nix
+++ pkgs/simul-cigri/default.nix
@@ -3,8 +3,8 @@ with pkgs;
 let
   src = fetchgit {
     url = "https://gitlab.inria.fr/cigri-ctrl/cigrisimul";
-    rev = "34e00bc763907d9113b708bfe33f409e4996c60e";
-    sha256 = "sha256-xp0JioezlGLI3Pw8XIDDwWfdgagBtcuXwMxKX3OPg2Q=";
+    rev = "0c0ce49f99124a2355ada44347b749f21d88cbc7";
+    sha256 = "sha256-tU4D0g2MxEWD2QnOtoXNSWGznylwtuqrWsNZ15+GyDU=";
   };
 
   rPkgs = with rPackages; [ tidyverse reshape2 gridExtra ];
@@ -21,7 +21,20 @@ in rec {
     verifyCargoDeps = true;
   };
 
-  sigri-sum = writeScriptBin "sigri-sum" ''
+  sigri-summary = writeScriptBin "sigri-summary" ''
     ${myR}/bin/Rscript ${src}/R/plot.R $1
   '';
+
+  sigri-docker = dockerTools.buildImage {
+    name = "guilloteauq/sigri";
+    tag = "latest";
+
+    contents = sigri;
+    config = {
+      # docker run -it --rm -v "$(pwd):/data" sigri experiments/mfc.yml
+      Entrypoint = [ "${sigri}/bin/cigri-simul" ];
+      WorkingDir = "/data";
+      Volumes = { "/data" = { }; };
+    };
+  };
 }
