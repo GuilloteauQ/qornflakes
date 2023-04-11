b0719eacf32c6597e3d6426dfc54fd624f66beaf
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-04-11 13:46:25 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -4,21 +4,17 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/22.11";
-    nbpreview-src.url = "github:GuilloteauQ/nbpreview";
-    marksman_nixpkgs.url = "github:stasjok/nixpkgs/marksman";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, nbpreview-src, marksman_nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     let templates = import ./templates/nix_flake_templates.nix;
     in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
         shellSet = import ./shells/default.nix { inherit pkgs; };
-        marksman = marksman_nixpkgs.legacyPackages.${system}.marksman;
       in rec {
-        packages = (import ./pkgs/packages.nix { inherit pkgs; }) // { inherit marksman; };
+        packages = (import ./pkgs/packages.nix { inherit pkgs; });
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
