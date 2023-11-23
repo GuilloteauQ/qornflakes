a730e02e735543e8609d4ec55a9a6c4042a63f2c
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-11-23 19:06:38 +0100

    oops

--- pkgs/lb4omp/default.nix
+++ pkgs/lb4omp/default.nix
@@ -1,15 +1,34 @@
-{ stdenv, FetchFromGitHub, cmake }:
+{ stdenv, fetchFromGitHub, cmake, perl, python3Packages, lit, llvmPackages, clang, pkg-config, libelf, libffi }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
     name = "LB4OMP";
     version = "2022-10-11";
-    src = FetchFromGitHub {
+    src = fetchFromGitHub {
         owner = "unibas-dmi-hpc";
         repo = name;
         rev = "00b3ecf8264e6ddc0d0ee277345ba466a4e4ea2f";
-        sha256 = "";
+        sha256 = "sha256-5fe3krt5KMnx0UXAvttUmGx2Ytlfqm64NAeTJ/SVz6g=";
     };
     nativeBuildInputs = [
        cmake
+       pkg-config
+    ];
+    cmakeFlags = [
+        "-DCMAKE_C_COMPILER=${clang}/bin/clang"
+        "-DCMAKE_CXX_COMPILER=${clang}/bin/clang++"
+        # "-DLIBOMP_ARCH=x86_64"
+        "-DLIBOMP_HAVE___RDTSC=1"
+    ];
+    buildInputs = [
+        perl
+        libelf libffi
+        clang
+        (python3Packages.python.withPackages (ps: with ps; [
+            lit
+            filecheck
+        ]))
+        lit
+        llvmPackages.openmp
+        llvmPackages.bintools-unwrapped
     ];
 }
