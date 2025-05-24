07e355a462c075cb3d650dadff254c1714e4e8de
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2025-05-24 12:34:04 +0200

    finally update this template flake...

--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -1,18 +1,16 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/25.05";
   };
+
+  nixConfig.bash-prompt = "(\\u@\\h) \\w [dev]\$ ";
 
   outputs = { self, nixpkgs }:
     let
-      #system = "x86_64-linux";
-      system = "aarch64-darwin";
+      system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
-
       devShells.${system} = {
         default = pkgs.mkShell {
           packages = with pkgs; [
@@ -20,6 +18,5 @@
           ];
         };
       };
-
     };
 }
