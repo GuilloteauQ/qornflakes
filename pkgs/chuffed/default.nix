c2d1b1b72c0989359bd4f05b2ce0bfadf8263eeb
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2025-11-26 19:28:51 +0100

    shorten version name of chuffed

--- pkgs/chuffed/default.nix
+++ pkgs/chuffed/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
     pname = "chuffed";
-    version = "ad5c46c9ea0ac524583807500a18fc21d8d87504";
+    version = "ad5c46c9";
     src = fetchFromGitHub {
         repo = pname;
         owner = pname;
-        rev = version;
+        rev = "ad5c46c9ea0ac524583807500a18fc21d8d87504";
         sha256 = "sha256-4euRjg2V3ZRuR4kG29O5gBNoj6Hzll0JF+xs+woD7l8=";
     };
     buildInputs = [
